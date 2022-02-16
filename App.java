b1b890b55850fbb362551c65a49443d8a0fabb39
Bognár Dávid <bognardavidmark@gmail.com>
2022-02-16 09:58:38 +0100

    Kezdes

---
+++ App.java
@@ -0,0 +1,24 @@
+/*
+* File: App.java
+* Author: Kovács Dorina
+* Copyright: 2022, Kovács Dorina
+* Group: Szoft I/N
+* Date: 2021-02-16
+* Github: https://github.com/Kdorina/
+* Licenc: GNU GPL
+*/
+import java.util.Scanner;
+
+class App{
+	public static void main(String[] args){
+		System.out.println("Kovács Dorina, 2022-02-16, Szoft I N");
+		System.out.println("Feladat 0306");
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Alap: ");
+		int alap = 
+		
+		
+		}
+	
+	}

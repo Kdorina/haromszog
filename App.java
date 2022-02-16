0f7f1dc5385f27e77fd13b94231a345789b25bdb
Kdorina <kovacsdorina2@gmail.com>
2022-02-16 10:11:32 +0100

    Kesz az elso

--- App.java
+++ App.java
@@ -11,12 +11,20 @@ import java.util.Scanner;
 
 class App{
 	public static void main(String[] args){
-		System.out.println("Kovács Dorina, 2022-02-16, Szoft I N");
+		System.out.println("Kovacs Dorina, 2022-02-16, Szoft I N");
 		System.out.println("Feladat 0306");
+		
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Alap: ");
-		int alap = 
+		double alap = sc.nextInt();
+		
+		System.out.println("Magassag: ");
+		double magassag = sc.nextInt();
+		
+		double eredmeny = (alap*magassag)/2;
+		
+		System.out.println("Terulet: " + eredmeny);
 		
 		
 		}
